23b8dd5dd49745a9495ed96618e5e62feb52e94a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-29 20:46:24 +0700

    Yeah

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,8 +5,27 @@ jest.mock("./sum", () => ({
   sum: () => 3,
 }));
 
-it("should work as expected", () => {
-  expect(sum(1, 2)).toBe(3);
+const mockFn = jest.fn();
 
-  expect(multiply(2, 3)).toBe(6);
+describe("Sample", () => {
+  beforeEach(() => {
+    // jest.resetAllMocks();
+  });
+
+  it("should work as expected", () => {
+    expect(sum(1, 2)).toBe(3);
+
+    expect(multiply(2, 3)).toBe(6);
+  });
+
+  it("should work first", () => {
+    mockFn();
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  it("should work second", () => {
+    mockFn.mockReset();
+    mockFn();
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
 });
